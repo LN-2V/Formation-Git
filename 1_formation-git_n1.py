1d7b558c78cdc54798af2092bde205050a1058e3
LN-2V <150041614+LN-2V@users.noreply.github.com>
2026-03-19 14:22:02 +0100

    Jardiniers et spécialisations

--- 1_formation-git_n1.py
+++ 1_formation-git_n1.py
@@ -1,1 +1,16 @@
 print ("Branch du jardin")
+
+class Terrain:
+    def __init__(self, jardiniers, arbres):
+        self.jardiniers = jardiniers
+        self.arbres = arbres
+    
+    def afficher(self):
+        print(self.jardiniers, self.arbres)
+
+
+variable = Terrain("Jean", "Chêne")
+
+variable.afficher()
+
+
